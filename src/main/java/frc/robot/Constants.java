26d20030604a246bdf37684dcb4bb2b504cb9d98
Dh-Van <dhvan17shah@gmail.com>
2023-06-13 12:42:45 +0000

    Added drive command to robotcontainer

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,5 +16,8 @@ public final class Constants {
   
     public static class OperatorConstants {
         public static final int kDriverControllerPort = 0;
+
+        public static final double LEFT_X_DEADBAND = 0.01;
+        public static final double LEFT_Y_DEADBAND = 0.01;
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,39 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.commands.TeleopDrive;
 
 public class RobotContainer {
 
+    private final Drivetrain m_drivetrain = new Drivetrain(new File(Filesystem.getDeployDirectory(), "swerve"));
+    CommandXboxController driverXbox = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
 	public RobotContainer() {
+
+        TeleopDrive closedFieldRel = new TeleopDrive(
+            m_drivetrain,
+            () -> MathUtil.applyDeadband(driverXbox.getLeftY(), OperatorConstants.LEFT_Y_DEADBAND),
+            () -> MathUtil.applyDeadband(driverXbox.getLeftX(), OperatorConstants.LEFT_X_DEADBAND),
+            () -> -driverXbox.getRawAxis(3), () -> true, false, true);
+
+
+        m_drivetrain.setDefaultCommand(closedFieldRel);
+
 		// Configure the trigger bindings
 		configureBindings();
 	}
 
 	private void configureBindings() {
-
+        driverXbox.back().onTrue(new InstantCommand(() -> m_drivetrain.zeroGyro()));
 	}
 
 	public Command getAutonomousCommand() {
