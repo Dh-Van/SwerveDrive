d8816fe5b839ec90f976d28175e07b0b91d8062a
Dh-Van <dhvan17shah@gmail.com>
2023-06-13 12:20:14 +0000

    Added the drivetrain subsystem with a basic drive command

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,16 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+
 public final class Constants {
+    
+    public static final double ROBOT_MASS = (152.89) * 0.453592; // 152.89lbs * kg per pound
+    public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(12)), ROBOT_MASS);
+    public static final double LOOP_TIME  = 0.2; //s, 20ms
+  
     public static class OperatorConstants {
         public static final int kDriverControllerPort = 0;
     }
